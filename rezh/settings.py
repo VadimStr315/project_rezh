a11174f7acfac6ec96e7861e76394b81d9ca4d00
VadimStr315 <67193820+VadimStr315@users.noreply.github.com>
2022-11-01 15:28:01 +0500

    Update settings.py
    fix setiings

--- rezh/settings.py
+++ rezh/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n@rf=5oz$%wx7q(pu=(mscxd+1+hrj)+dm6hya_m=_@z*i*&it'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
